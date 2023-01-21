1892947832fa29d42f81500319c6bae4dc921262
AndreyZhiryakov <aoj1976@yandex.ru>
2023-01-21 19:36:01 +0300

    check1
    check1

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -36,19 +36,19 @@ import web.service.UserService;
             return "redirect:/users";
         }
 
-        @GetMapping("/edit/{id}")
+        @GetMapping("/users/{id}")
         public String getFormUserUpdate(Model model,@PathVariable("id") int id){
             model.addAttribute("user",userService.getUserById(id));
             return "edit";
         }
 
-        @PatchMapping("/edit/{id}")
+        @PatchMapping("/users/{id}")
         public String updateUser (@ModelAttribute("user") User user,@PathVariable("id")int id){
             userService.updateUser(id,user);
             return "redirect:/users";
         }
 
-        @DeleteMapping("/edit/{id}")
+        @DeleteMapping("/users/{id}")
         public String deleteUser(@PathVariable("id") int id){
             userService.deleteUser(id);
             return "redirect:/users";
